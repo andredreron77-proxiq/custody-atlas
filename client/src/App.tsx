86ea8db7cda872ea6c5b58df4efb6568bbe678c9
Andre Oliver <andre.dreron77@gmail.com>
2026-04-18 16:43:41 -0400

    fix: move QueryClientProvider to root to fix blank screen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,5 @@
 import { Switch, Route, useLocation } from "wouter";
 import { useEffect } from "react";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
 import { useQuery } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -211,7 +209,6 @@ function App() {
 
   return (
     <ThemeProvider>
-    <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <ScrollToTop />
         <div className="min-h-screen flex flex-col">
@@ -226,7 +223,6 @@ function App() {
         <OnboardingModal />
         <Toaster />
       </TooltipProvider>
-    </QueryClientProvider>
     </ThemeProvider>
   );
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,14 @@
 import { createRoot } from "react-dom/client";
+import { QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
+import { queryClient } from "./lib/queryClient";
 import "./index.css";
 import { initAnalytics } from "./lib/analytics";
 
 initAnalytics();
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>,
+);
